f3b93961b21262758811055c5406679a7cea6caf
Asya Simonyan <anniesim97@gmail.com>
2017-04-22 00:55:06 +0300

    2d cmt added test page

--- french_rafl_learners.py
+++ french_rafl_learners.py
@@ -8,20 +8,34 @@ app = Flask(__name__)
 def material_page():
     profile_refer = url_for('profile_page')
     quizes_refer = url_for('quizes_page')
-    return render_template('vocab.html', profile_refer=profile_refer, quizes_refer=quizes_refer)
+    test_refer = url_for('test_page')
+    return render_template('vocab.html',
+                           profile_refer=profile_refer, quizes_refer=quizes_refer, test_refer=test_refer)
 
 
 @app.route('/profile_page')
 def profile_page():
     materials_refer = url_for('material_page')
     quizes_refer = url_for('quizes_page')
-    return render_template('profile_page.html', materials_refer=materials_refer, quizes_refer=quizes_refer)
+    return render_template('profile_page.html',
+                           materials_refer=materials_refer, quizes_refer=quizes_refer)
+
 
 @app.route('/quizes_page')
 def quizes_page():
     materials_refer = url_for('material_page')
     profile_refer = url_for('profile_page')
-    return render_template('quizes_page.html', materials_refer=materials_refer, profile_refer=profile_refer)
+    return render_template('quizes_page.html',
+                           materials_refer=materials_refer, profile_refer=profile_refer)
+
+
+@app.route('/test_page')
+def test_page():
+    profile_refer = url_for('profile_page')
+    materials_refer = url_for('material_page')
+    quizes_refer = url_for('quizes_page')
+    return render_template('test_page.html',
+                           profile_refer=profile_refer, materials_refer=materials_refer, quizes_refer=quizes_refer)
 
 if __name__ == '__main__':
     app.run(debug=True)
