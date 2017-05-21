f7e4470b414bc6c47e46e4bb9a160b9ff019f40d
Asya Simonyan <anniesim97@gmail.com>
2017-05-21 14:43:08 +0300

    added a test on verb conjugation

--- french_rafl_learners.py
+++ french_rafl_learners.py
@@ -14,6 +14,26 @@ app.secret_key = os.urandom(24)
 # todo: сделать собирание тестов на более раннем этапе
 
 
+def distance(a, b):
+    # "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a, b = b, a
+        n, m = m, n
+
+    current_row = range(n+1) # Keep current and previous row, not entire matrix
+    for i in range(1, m+1):
+        previous_row, current_row = current_row, [i]+[0]*n
+        for j in range(1,n+1):
+            add, delete, change = previous_row[j]+1, current_row[j-1]+1, previous_row[j-1]
+            if a[j-1] != b[i-1]:
+                change += 1
+            current_row[j] = min(add, delete, change)
+
+    return current_row[n]
+
+
 def call_db(tbl):
     class Words(object):
         pass
@@ -36,7 +56,7 @@ def voc_maker(ps, categories):
     return d
 
 
-def sorting(data, ps, categories):
+def sorting(data, ps, categories, f=None):
     #print('sorting')
     d = {}
     for row in data:
@@ -50,7 +70,7 @@ def sorting(data, ps, categories):
     return d
 
 
-def sorting_back(data, ps, categories):
+def sorting_back(data, ps, categories, f=None):
     #print('sorting')
     d = {}
     for row in data:
@@ -64,8 +84,8 @@ def sorting_back(data, ps, categories):
     return d
 
 
-def genpl_sorting(data, ps, categories):
-    forms = ex_genpl_maker()
+def gramm_sorting(data, ps, categories, f):
+    forms = f()
     d = {}
     for row in data:
         if row['part_of_speech'] == ps:
@@ -75,24 +95,35 @@ def genpl_sorting(data, ps, categories):
                         d[category] = []
                     if row['Rus'] in forms:
                         d[category].append([forms[row['Rus']][0], forms[row['Rus']][1]])
-    # print('sorted')
     return d
 
 
 def ex_genpl_maker():
     result = call_db('s_decl')
     d = {}
-    arr = ['Много ...', 'Мало ...', 'Не осталось ...', 'Не хватает ...']
+    context = ['Много ...', 'Мало ...', 'Не осталось ...', 'Не хватает ...']
     for row in result:
         if row['gen_pl'] != '' and row['gen_pl'] is not None:
-            d[row['nom_sg']] = [str(random.choice(arr)) + ' (' + row['nom_sg'] + ')', row['gen_pl']]
+            d[row['nom_sg']] = [str(random.choice(context)) + ' (' + row['nom_sg'] + ')', row['gen_pl']]
     return d
 
 
-def quiz_maker(ps, cat, func):
+def prs_conj_maker():
+    result = call_db('v_conj')
+    d = {}
+    arr = {'Я ... (': 'prs1sg', 'Ты ... (': 'prs2sg', 'Он ... (': 'prs3sg', 'Она ... (': 'prs3sg',
+               'Мы ... (': 'prs1pl', 'Вы ... (': 'prs2pl', 'Они ... (': 'prs3pl'}
+    for row in result:
+        if row['prs1sg'] != '' and row['prs1sg'] is not None:
+            context = random.choice(list(arr))
+            d[row['inf']] = [context + row['inf'] + ')', row[arr[context]]]
+    return d
+
+
+def quiz_maker(ps, cat, sorter, func=None):
     #print('quizmaker ON')
     result = call_db('rus_words')
-    d = func(result, ps, cat)
+    d = sorter(result, ps, cat, func)
     newd = {}
     for key in d:#{'1d':[['лол', 'lol'], ['шта', 'wut']]}
         try:
@@ -242,6 +273,38 @@ def test_gen(categ):
     except NameError:
         redirect(url_for('vocab_adverbs'))
 
+#todo: добавить подсказки правил
+@app.route('/materials/conj_<categ>', methods=['GET', 'POST'])
+def test_conj(categ):
+    # questions_adv = session['quest_b_adv']
+    questions = session['ex_conj']
+    try:
+        profile_refer = url_for('profile_page')
+        quizes_refer = url_for('quizes_page')
+        if request.method == "POST":
+            entered_answer = request.form.get('answer', '')
+            if not entered_answer:
+                flash("Please enter an answer", "error")  # Show error if no answer entered
+            elif entered_answer.replace('́', '&#769;') != questions[categ][session["current_question"]]["answer"]:
+                flash("La bonne réponse:\n" + questions[categ][session["current_question"]]["answer"],
+                      "error")
+            else:
+                session["current_question"] = str(int(session["current_question"]) + 1)
+                if session["current_question"] in questions:
+                    redirect(url_for('conj_', categ=categ))
+                else:
+                    return render_template("success.html", profile_refer=profile_refer, quizes_refer=quizes_refer, a=0)
+        if "current_question" not in session:
+            session["current_question"] = "1"
+        elif session["current_question"] not in questions[categ]:
+            session.pop("current_question")
+            return render_template("success.html", profile_refer=profile_refer, quizes_refer=quizes_refer, a=1)
+        return render_template("test_conj.html",
+                               question=questions[categ][session["current_question"]]["question"],
+                               question_number=session["current_question"],
+                               profile_refer=profile_refer, quizes_refer=quizes_refer, cat=categ)
+    except NameError:
+        redirect(url_for('vocab_adverbs'))
 """------------------------------------------------------------------------"""
 
 
@@ -251,7 +314,7 @@ def vocab_nouns():
     cat = ['1d', 'm', 'n', '3d', 'sg_tantum', 'pl_tantum']
     session['questions_n'] = quiz_maker(ps, cat, sorting)
     session['quest_b_n'] = quiz_maker(ps, cat, sorting_back)
-    session['ex_genpl'] = quiz_maker(ps, cat, genpl_sorting)
+    session['ex_genpl'] = quiz_maker(ps, cat, gramm_sorting, ex_genpl_maker)
     voc = voc_maker(ps, cat)
     profile_refer = url_for('profile_page')
     quizes_refer = url_for('quizes_page')
@@ -268,6 +331,7 @@ def vocab_verbs():
     cat = ['1_productif', '1_sans_diff', '1_avec_diff', '1_base_altern', '2_productif', '2_improductif']
     session['questions_v'] = quiz_maker(ps, cat, sorting)
     session['quest_b_v'] = quiz_maker(ps, cat, sorting_back)
+    session['ex_conj'] = quiz_maker(ps, cat, gramm_sorting, prs_conj_maker)
     voc = voc_maker(ps, cat)
     profile_refer = url_for('profile_page')
     quizes_refer = url_for('quizes_page')
@@ -296,6 +360,6 @@ def vocab_adverbs():
 #def keyboard():
  #   return render_template('keyb.html')
 
-ex_genpl_maker()
+
 if __name__ == '__main__':
     app.run(debug=True)
